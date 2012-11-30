69aa051c0f5f66aa21b3cf3ed21edd7990ae95f2
Thomas Chen <foxnewsnetwork@gmail.com>
2012-11-30 08:14:23 -0800

    initial commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unfactory::Application.config.secret_token = '3d768fad5e647a40327a787ddc5d097bf56882ba2a471c4eca989f8cef59a56bfbc1767181739323a3f6660aecb9617415f5c2b5e0e41bc6c988c4b08dd9223e'
